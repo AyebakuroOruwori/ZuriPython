f1ff71ec0c88ed71f63a54e42b54925022900c21
AyebakuroOruwori <ayebakurooruwori@gmail.com>
2021-04-23 01:21:41 +0100

    DjangoApp

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
-from .import views
+from . import views
 
 urlpatterns = [
 
-    url('^$', views.index, name ="index"),
+    url('^$', views.index, name="index"),
     #127.0.0.1/polls/
-
+    url('^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+    #127.0.0.1/polls/1
+    url('^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
+    #127.0.0.1/polls/1/results
+    url('^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
+    #127.0.0.1/Polls/1/vote
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse('Awesome job guys! This is the index page, of our polls application')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context ={'latest_questions':latest_questions}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view of the question: %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question:                                                      %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: %s" % question_id)
+
+
