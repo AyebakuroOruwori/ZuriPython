6a87f43570ea95ba0aca6fc585e40988a4b8caa4
AyebakuroOruwori <ayebakurooruwori@gmail.com>
2021-04-28 00:25:01 +0100

    DjangoApp

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,11 +5,12 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context ={'latest_questions':latest_questions}
+    context ={'latest_questions': latest_questions}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("This is the detail view of the question: %s" % question_id)
+    question = Question.objects.get(pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("These are the results of the question:                                                      %s" % question_id)
